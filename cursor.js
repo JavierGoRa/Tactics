a39f8f80f7da4574ebde3f73e2a214baace62153
JavierGoRa <35291605+JavierGoRa@users.noreply.github.com>
2025-07-09 16:06:52 +0200

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -6,22 +6,22 @@ function controllersCursorOnBoard(event){
     //Key A
     if (keyCode == 65) {
         camera.position.x = camera.position.x - 1;
-        console.log(backX + ' ' + backY);
+        camera.position.z = camera.position.z + 1;
     }
     //Key W
     if (keyCode == 87) {
+        camera.position.x = camera.position.x - 1;
         camera.position.z = camera.position.z - 1;
-        console.log(backX + ' ' + backY);
     }
     //Key D
     if (keyCode == 68) {
+        camera.position.z = camera.position.z - 1;
         camera.position.x = camera.position.x + 1;
-        console.log(backX + ' ' + backY);
     }
     //Key S
     if (keyCode == 83) {
+        camera.position.x = camera.position.x + 1;
         camera.position.z = camera.position.z + 1;
-        console.log(backX + ' ' + backY);
     }
     //Key O
     if (keyCode == 79) {
@@ -50,22 +50,17 @@ function controllersCursorOnBoard(event){
 
 function moveActionsToCursor(X, Z){
 
-    var moveAction = arrayActions.find(tempVar => tempVar.name == 'actionMove');
+    var moveAction = arrayActions.find(tempVar => tempVar.prop == 'actionMove');
 
     moveAction.position.x = X - 2;
     moveAction.position.z = Z - 0.5;
 
-    var fightAction = arrayActions.find(tempVar => tempVar.name == 'actionFight');
+    var fightAction = arrayActions.find(tempVar => tempVar.prop == 'actionFight');
 
     fightAction.position.x = X - 1;
     fightAction.position.z = Z - 0.5;
 
-    var fightAction = arrayActions.find(tempVar => tempVar.name == 'actionShop');
-
-    fightAction.position.x = X - 0;
-    fightAction.position.z = Z - 0.5;
-
-    var fightAction = arrayActions.find(tempVar => tempVar.name == 'actionWait');
+    var fightAction = arrayActions.find(tempVar => tempVar.prop == 'actionWait');
 
     fightAction.position.x = X + 1;
     fightAction.position.z = Z - 0.5;
@@ -77,4 +72,79 @@ function moveActionsToCursor(X, Z){
 
 
 
+}
+
+
+function generateActions(){
+
+    if(arrayActions.length > 0){
+                
+        arrayActions.forEach(e => {
+            scene.remove(e);
+        });
+        arrayActions = [];
+
+    }
+        
+    var bat = new THREE.MeshBasicMaterial({ map:THREE.ImageUtils.loadTexture( (arrayEndsActions.includes('Movement')) ? 'src/textures/moveOff.png' : 'src/textures/move.png' )});
+    bat.map.needsUpdate = true; //ADDED
+    
+    // plane
+    batMesh = new THREE.Mesh(new THREE.CircleGeometry( 0.5, 6 ),bat);
+    batMesh.position.set( 5, 11, 6 );
+    batMesh.rotation.x = -0.35;
+    batMesh.rotation.y = 0.75;
+    batMesh.rotation.z = 0.25;
+    batMesh.overdraw = true;
+    batMesh.prop = 'actionMove';
+    batMesh.name = (arrayEndsActions.includes('Movement')) ? 'actionMoveOff' : 'actionMove' ;
+    
+    scene.add(batMesh);
+
+    arrayActions.push(batMesh);
+    
+    renderer.render( scene, camera );
+
+    var bat = new THREE.MeshBasicMaterial({ //CHANGED to MeshBasicMaterial
+        map:THREE.ImageUtils.loadTexture((arrayEndsActions.includes('Attack')) ? 'src/textures/fightOff.png' : 'src/textures/fight.png')
+    });
+    bat.map.needsUpdate = true; //ADDED
+    
+    // plane
+    batMesh = new THREE.Mesh(new THREE.CircleGeometry( 0.5, 6 ),bat);
+    batMesh.position.set( 6, 11.5, 6 );
+    batMesh.rotation.x = -0.35;
+    batMesh.rotation.y = 0.75;
+    batMesh.rotation.z = 0.25;
+    batMesh.overdraw = true;
+    batMesh.prop = 'actionFight';
+    batMesh.name = (arrayEndsActions.includes('Attack')) ? 'actionFightOff' : 'actionFight' ;
+    
+
+    scene.add(batMesh);
+    
+    arrayActions.push(batMesh);
+    
+    renderer.render( scene, camera );
+
+    
+    var bat = new THREE.MeshBasicMaterial({ //CHANGED to MeshBasicMaterial
+        map:THREE.ImageUtils.loadTexture('src/textures/next.png')
+    });
+    bat.map.needsUpdate = true; //ADDED
+    
+    // plane
+    batMesh = new THREE.Mesh(new THREE.CircleGeometry( 0.5, 6 ),bat);
+    batMesh.position.set( 8, 12.5, 6 );
+    batMesh.rotation.x = -0.35;
+    batMesh.rotation.y = 0.75;
+    batMesh.rotation.z = 0.25;
+    batMesh.overdraw = true;
+    batMesh.name = 'actionWait';
+    batMesh.prop = 'actionWait';
+    scene.add(batMesh);
+
+    arrayActions.push(batMesh);
+    
+    renderer.render( scene, camera );
 }
